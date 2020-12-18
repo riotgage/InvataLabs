c80b528d319e73f4c73fee9310b3fc585e365a74
rohit <rvsatwadhar@gmail.com>
2020-12-18 23:42:39 +0530

    Added User login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,12 @@
 const User=require('../models/User')
 const errorResponse=require('../utils/errorResponse');
 const path=require('path');
-
+const colors=require('colors')
 //@desc Register User
-//@Route Get /api/v1/auth/register
+//@Route POST /api/v1/auth/register
 //@access Public
 
-exports.register=async(req,res,next)=>{
+exports.register=async(req,res,next)=>{ 
     try{
         const {name,email,password,role}=req.body;
         
@@ -18,8 +18,46 @@ exports.register=async(req,res,next)=>{
             role
         });
 
+        // Create token
+        const token=user.getJWTSignature()
         res.status(200).json({
-            success:true
+            success:true,
+            token
+        })
+    }catch(error){
+        next(error);
+    }
+}
+
+//@desc Login User
+//@Route POST /api/v1/auth/login
+//@access Public
+
+exports.login=async(req,res,next)=>{
+    try{
+        const {email,password}=req.body; 
+        if(!email || !password){
+            return next(new errorResponse(`Email and Password are required`,400))
+        }
+
+        const user=await User.findOne({email}).select('+password'); 
+
+        if(!user){
+            return next(new errorResponse(`Credentials are invalid`,401))
+        }
+        const isMatch= await user.authPassword(password); 
+        console.log(isMatch.yellow)
+ 
+        if(!isMatch){
+            return next(new errorResponse(`Credentials are invalid`,401))
+        }   
+        
+        // Create token
+        const token=user.getJWTSignature()
+
+        res.status(200).json({
+            success:true,
+            token
         })
     }catch(error){
         next(error);
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose');
 const bcrypt=require('bcryptjs');
-
+const jwt=require('jsonwebtoken');
 const UserSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -43,4 +43,19 @@ UserSchema.pre('save',async function(next){
     const salt=await bcrypt.genSalt(10);
     this.password=await bcrypt.hash(this.password,salt);
 })
+
+// JWT signature
+UserSchema.methods.getJWTSignature=()=>{
+    return jwt.sign({
+        id:this._id,   
+    },process.env.JWT_SECRET_KEY,{
+        expiresIn:process.env.JWT_EXPIRE
+    })
+} 
+
+// Authenticate User with password and hashed password in db
+UserSchema.methods.authPassword=async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password)
+} 
+
 module.exports = mongoose.model('User',UserSchema);
